b1b9bb4b083532131bfd497516bbb956ee87f5d6
Coding-Robin001 <codingade@gmail.com>
2024-09-06 18:39:53 +0100

    coommitt

--- springBoot-tutorial/src/main/java/com/springBootProject/springBoot/tutorial/service/DepartmentService.java
+++ springBoot-tutorial/src/main/java/com/springBootProject/springBoot/tutorial/service/DepartmentService.java
@@ -1,4 +1,7 @@
 package com.springBootProject.springBoot.tutorial.service;
 
+import com.springBootProject.springBoot.tutorial.entity.Department;
+
 public interface DepartmentService {
+    public Department saveDepartment(Department department);
 }
--- springBoot-tutorial/src/main/java/com/springBootProject/springBoot/tutorial/service/DepartmentServiceImplementation.java
+++ springBoot-tutorial/src/main/java/com/springBootProject/springBoot/tutorial/service/DepartmentServiceImplementation.java
@@ -1,7 +1,18 @@
 package com.springBootProject.springBoot.tutorial.service;
 
+import com.springBootProject.springBoot.tutorial.entity.Department;
+import com.springBootProject.springBoot.tutorial.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DepartmentServiceImplementation implements DepartmentService{
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
 }
