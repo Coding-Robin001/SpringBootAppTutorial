40d5a038f8c55849aa3be356feb449d504b689ba
Coding-Robin001 <codingade@gmail.com>
2024-09-06 17:30:15 +0100

    commit

--- springBoot-tutorial/src/main/java/com/springBootProject/springBoot/tutorial/repository/DepartmentRepository.java
+++ springBoot-tutorial/src/main/java/com/springBootProject/springBoot/tutorial/repository/DepartmentRepository.java
@@ -1,7 +1,9 @@
 package com.springBootProject.springBoot.tutorial.repository;
 
+import com.springBootProject.springBoot.tutorial.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartmentRepository {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
