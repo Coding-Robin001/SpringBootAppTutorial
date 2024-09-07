02d3ac3254a6c98be49260a110a716ab1e4c026c
Coding-Robin001 <codingade@gmail.com>
2024-09-07 14:11:51 +0100

    commit

--- springBoot-tutorial/src/main/java/com/springBootProject/springBoot/tutorial/service/DepartmentService.java
+++ springBoot-tutorial/src/main/java/com/springBootProject/springBoot/tutorial/service/DepartmentService.java
@@ -2,6 +2,10 @@ package com.springBootProject.springBoot.tutorial.service;
 
 import com.springBootProject.springBoot.tutorial.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
 }
