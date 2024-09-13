43bba53a9d182912abbf89aad011e4dbb13084c6
Coding-Robin001 <codingade@gmail.com>
2024-09-13 16:27:13 +0100

    commit

---
+++ springBoot-tutorial/src/test/java/com/springBootProject/springBoot/tutorial/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.springBootProject.springBoot.tutorial.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
