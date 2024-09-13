b2b2985c90e38c55a45b911241db684be6f34ed2
Coding-Robin001 <codingade@gmail.com>
2024-09-13 17:11:33 +0100

    commit

--- springBoot-tutorial/src/test/java/com/springBootProject/springBoot/tutorial/repository/DepartmentRepositoryTest.java
+++ springBoot-tutorial/src/test/java/com/springBootProject/springBoot/tutorial/repository/DepartmentRepositoryTest.java
@@ -1,12 +1,33 @@
 package com.springBootProject.springBoot.tutorial.repository;
 
+import com.springBootProject.springBoot.tutorial.entity.Department;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class DepartmentRepositoryTest {
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
 
     @BeforeEach
     void setUp() {
+        Department department =
+                new Department(41L, "ICS", "ABUJA", "1824");
+        entityManager.persist(department);
+    }
+
+    @Test
+    public void whenFindById_thenReturnDepartment(){
+        Department department = departmentRepository.findById(41L).get();
+        assertEquals(department.getDepartmentName(), "ICS");
     }
 }
