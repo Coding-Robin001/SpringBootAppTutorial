6d08842fb508dc20516c0fd6a0f7044852f13c08
Coding-Robin001 <codingade@gmail.com>
2024-09-16 11:46:53 +0100

    createst test for getDepartmentById

--- springBoot-tutorial/src/test/java/com/springBootProject/springBoot/tutorial/controler/DepartmentControllerTest.java
+++ springBoot-tutorial/src/test/java/com/springBootProject/springBoot/tutorial/controler/DepartmentControllerTest.java
@@ -3,7 +3,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import com.jayway.jsonpath.JsonPath;
 import com.springBootProject.springBoot.tutorial.entity.Department;
+import com.springBootProject.springBoot.tutorial.error.DepartmentNotFoundException;
 import com.springBootProject.springBoot.tutorial.service.DepartmentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +19,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.print.attribute.standard.Media;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,23 +38,28 @@ class DepartmentControllerTest {
     @BeforeEach
     void setUp() {
         Department department =
-                new Department(71L, "TSC", "BAYELSA", "1392");
+                new Department(1L, "TSC", "BAYELSA", "1392");
     }
 
     @Test
     void saveDepartment () throws Exception {
         Department departmentInput =
-                new Department(71L, "TSC", "BAYELSA", "1392");
+                new Department(1L, "TSC", "BAYELSA", "1392");
         Mockito.when(departmentService.saveDepartment(departmentInput)).thenReturn(department);
-        String jsonRequestBody = "{ \"departmentId\": \"71L\", \"departmentName\": \"TSC\", \"departmentAddress\": \"BAYELSA\", \"departmentCode\": \"1392\"}";
+        String jsonRequestBody = "{ \"departmentName\": \"TSC\", \"departmentAddress\": \"BAYELSA\", \"departmentCode\": \"1392\" }";
 
-        mockMvc.perform(post("/departments")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonRequestBody)
-        ).andExpect(status().isOk());
+
+        mockMvc.perform(post("/departments").contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequestBody)).andExpect(status().isOk());
     }
 
     @Test
-    void fetchDepartmentById() {
+    void fetchDepartmentById() throws Exception {
+        Mockito.when(departmentService.fetchDepartmentById(71L))
+                .thenReturn(department);
+
+        mockMvc.perform(get("/departments/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
